4e3398ab5d51bb59453433a81a31ddba0ed134ab
ayrcoderhouse <130808160+ayrcoderhouse@users.noreply.github.com>
2023-04-14 13:07:54 -1200

    Add files via upload

---
+++ src/js/jsonSeeder.js
@@ -0,0 +1,17 @@
+jsonSeeder = {
+    "products":[
+        {"id":1,"title":"Watermelon","img":"src/img/watermelon.jpg","price":4.65,"description":"Whole watermelon about 6kg","stockQty":1000,"category":["Fruits"]},
+        {"id":2,"title":"Apricot","img":"src/img/apricot.jpg","price":1.80,"description":"Basket of apricots 6 units","stockQty":1000,"category":["Fruits"]},
+        {"id":3,"title":"Melon","img":"src/img/melon.jpg","price":4.00,"description":"Whole melon about 1.5kg","stockQty":1000,"category":["Fruits"]},
+        {"id":4,"title":"Tangerine","img":"src/img/tangerine.jpg","price":1.10,"description":"Tangerines mesh 2kg","stockQty":1000,"category":["Fruits","Organics"]},
+    ],
+    "categories":[
+        {"id":1,"name":"Fruits","color":"blue"},
+        {"id":2,"name":"Vegetables","color":"green"},
+        {"id":3,"name":"Juices","color":"orange"},
+        {"id":4,"name":"Organics","color":"yellow"}
+    ],
+    "admins":[
+        {"id":1,"name":"admin","surname":"administrator","email":"master","password":"1234"}
+    ]
+}
